c1b11a402ebed50b6a2fabc8aed2f62799fef340
ARTHURVANBELLE <23319@ecam.be>
2024-10-25 00:40:08 +0200

    created example pages

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import DB_tables  #this execute the creation of the tables
 
 # If u're looking for the code to create the tables ==> DB_tables.py
 
-#visit these pages http://127.0.0.1:5000/user - http://127.0.0.1:5000/login - http://127.0.0.1:5000/clan
+#visit these pages http://127.0.0.1:5000/user - http://127.0.0.1:5000/login - http://127.0.0.1:5000/clan - ...
 
 app = Flask(__name__)
 
@@ -14,13 +14,35 @@ def home():
 
 # Route for an user page 
 @app.route('/user')
-def about():
-    return render_template('user.html')
+def user_profile():
+    user = {
+        "clan": "Warriors of Light",
+        "money": 1500,
+        "inventory": ["Sword", "Shield", "Potion"],
+        "xp": 2450,
+        "elo": 1200,
+        "classe": "Warrior"
+    }
+    return render_template('user.html', user=user)
 
 # Route for an clan page 
 @app.route('/clan')
-def clan():
-    return render_template('clan.html')
+def clan_overview():
+    clan_members = [
+        {"username": "warrior123", "name": "John Smith", "money": 2000, "level": 35},
+        {"username": "mageQueen", "name": "Emily Stone", "money": 3500, "level": 42}
+    ]
+    return render_template('clan.html', clan_members=clan_members)
+
+# Route for the create_item page
+@app.route('/create_item')
+def create_item():
+    return render_template('create_item.html')
+
+# Route for the create_monster page
+@app.route('/create_monster')
+def create_monster():
+    return render_template('create_monster.html')
 
 
 if __name__ == '__main__':
