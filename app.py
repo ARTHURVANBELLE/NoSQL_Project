3f8aae856692198a18c452320598a41630fda0ba
ARTHURVANBELLE <23319@ecam.be>
2024-11-01 00:40:18 +0100

    begin inventory

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import dynamoConnect
 
 # Import the users_namespace from users.py
 from users import users_namespace
-
+from inventories import inventory_namespace
 from monsters import monsters_namespace
 
 app = Flask(__name__)
@@ -14,11 +14,13 @@ api = Api(app, version='1.0', title='Game API', description='API for the game ma
 
 # Register the User API namespace
 api.add_namespace(users_namespace, path='/users_api')
+api.add_namespace(monsters_namespace, path='/monsters_api')
+api.add_namespace(inventory_namespace, path='/inventory_api')
 
 # with app.app_context():
 #     print("Registered routes:")
 #     for rule in app.url_map.iter_rules():
-#         print(f"{rule.methods} {rule.rule}")
+#         print(f"{rule.methods} {rule.rule}") 
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ inventories.py
@@ -0,0 +1,47 @@
+from flask import request, render_template, jsonify, make_response
+from flask_restx import Api, Resource, fields, Namespace
+import boto3
+from botocore.exceptions import ClientError
+
+import dynamoConnect
+
+import uuid
+from decimal import Decimal
+
+# Initialize the Namespace (not an Api)
+inventory_namespace = Namespace('v1/inventory', description='Inventory Operation')
+inventory_table = dynamoConnect.dynamodb_resource.Table("inventories")
+
+# Define the user model for the Swagger UI
+inventory_model = inventory_namespace.model('Inventory', {
+    'inventory_id': fields.String(required=True, description='Unique user identifier'),
+    'item_id_list': fields.String(required=True, description='User name')
+})
+
+@inventory_namespace.route('/<string:inventory_id>')
+@inventory_namespace.param('inventory_id', 'The inventory identifier')
+
+class Inventory(Resource):
+    @inventory_namespace.doc('get_inventory')
+    def get(self, inventory_id):
+        """Fetch an inventory by ID"""
+        try:
+            response = inventory_table.get_item(Key={'inventory_id': inventory_id})
+            inventory = response.get('Item')
+            inventory_data = {
+                "invntory_id": inventory_id,
+                "item_id_list": inventory.get('item_id_list', "Unknown"),
+            }
+            if not inventory_data:
+                return {'message': 'Inventory not found'}, 404
+            
+            html_content = render_template('inventory.html', inventory=inventory_data)
+            response = make_response(html_content)
+            response.headers['Content-Type'] = 'text/html'
+            return response
+        except ClientError as e:
+            #return {'message': str(e)}, 500
+            html_content = render_template('inventory.html')
+            response = make_response(html_content)
+            response.headers['Content-Type'] = 'text/html'
+            return response
